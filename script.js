e877d2004d026f8bf46632e374171142088dab35
Burak <avniburakcitlak@gmail.com>
2022-07-12 13:21:36 +0300

    singleRound play added

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function computerPlay(){
     return choices[which];
 }
 
+function singleRound(playerSelection,computerSelection){
+    playerSelection=playerSelection.toLowerCase();
+    computerSelection=computerSelection.toLowerCase();
+    if((playerSelection===computerSelection)){
+        return "It's a tie!";
+    }
+    else if((playerSelection==='rock' && computerSelection==='paper')||(playerSelection==='paper'&& computerSelection==='scissors')||(playerSelection==='scissors' && computerSelection==='rock')){
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+    else{
+        return `You won! ${playerSelection} beats ${computerSelection}`;
+    }
+}
