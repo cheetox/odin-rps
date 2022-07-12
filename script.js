38e09fecffcddfd6049ca172ba18a0bd76730d17
Burak <avniburakcitlak@gmail.com>
2022-07-12 13:43:09 +0300

    added overall winner

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ function singleRound(playerSelection,computerSelection){
     playerSelection=playerSelection.toLowerCase();
     computerSelection=computerSelection.toLowerCase();
     if((playerSelection===computerSelection)){
-        return "It's a tie!";
+        return 2;
     }
     else if((playerSelection==='rock' && computerSelection==='paper')||(playerSelection==='paper'&& computerSelection==='scissors')||(playerSelection==='scissors' && computerSelection==='rock')){
-        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+       
+        return 1;
     }
     else{
-        return `You won! ${playerSelection} beats ${computerSelection}`;
+        res=true;
+        return 0;
+        
     }
 }
 
 function game(){
+    let playerScore=0;
+    let computerScore=0;
+
     for (let i = 0; i < 5; i++) {
         playerSelection=prompt('Rock, paper, scissors?');
         computerSelection=computerPlay();
-        console.log(singleRound(playerSelection,computerSelection));        
+        res=(singleRound(playerSelection,computerSelection));
+        if(res==0){
+            playerScore++;
+            console.log( `You won! ${playerSelection} beats ${computerSelection}`);
+        }
+        else if(res==1){
+            computerScore++;
+            console.log( `You lost! ${computerSelection} beats ${playerSelection}`);
+        }else if(res==2){
+            console.log("It's a tie!")
+        }        
+    }
+    if(playerScore>computerScore){
+        console.log('You won!');
+    }else if(playerScore<computerScore){
+        console.log('You lost!');
+    }else{
+        console.log('Wow!It\'s a tie!');
     }
 }
