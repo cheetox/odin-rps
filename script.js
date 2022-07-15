5e46c8ac0928b2a46b5bdfeb404bf3c7778a212f
Burak <avniburakcitlak@gmail.com>
2022-07-15 17:49:53 +0300

    added all of the functionality

--- script.js
+++ script.js
@@ -21,24 +21,65 @@ function singleRound(playerSelection,computerSelection){
     }
 }
 
-function game(){
+window.onload=function(){
     let playerScore=0;
     let computerScore=0;
+    const scores=document.querySelector('.scores');
+    const blocks=document.querySelectorAll('.ch');
+    const result=document.createElement('h2');
+    const you=document.querySelector('.you');
+    const computer=document.querySelector('.computer');
+    scores.appendChild(result);
+
+    
+    blocks.forEach((block) =>{
+        block.addEventListener('click',()=>{
+            
+            
+            
+            
+            
+            const computerSelection=computerPlay();
+            const res=singleRound(block.id,computerSelection);
+            if(res==0){
+                playerScore++;
+                result.textContent=`You won! ${block.id} beats ${computerSelection}`;
+               
+                
+            }
+            else if(res==1){
+                computerScore++;
+                result.textContent= `You lost! ${computerSelection} beats ${block.id}`;
+                
+            }else if(res==2){
+                result.textContent="It's a tie!";
+                
+            } 
+            
+            if(playerScore===5){
+                result.textContent='You WON!';
+                scores.appendChild(result);
+            }else if(computerScore===5){
+                result.textContent='You LOST!';
+                
+            }
+            you.textContent=playerScore;
+            computer.textContent=computerScore;
+            scores.classList.add('show');
+            
+        })
+    })
+
+};
+
+function game(){
+    
 
     for (let i = 0; i < 5; i++) {
         playerSelection=prompt('Rock, paper, scissors?');
         computerSelection=computerPlay();
         res=(singleRound(playerSelection,computerSelection));
-        if(res==0){
-            playerScore++;
-            console.log( `You won! ${playerSelection} beats ${computerSelection}`);
-        }
-        else if(res==1){
-            computerScore++;
-            console.log( `You lost! ${computerSelection} beats ${playerSelection}`);
-        }else if(res==2){
-            console.log("It's a tie!")
-        }        
+        
     }
     if(playerScore>computerScore){
         console.log('You won!');
@@ -48,3 +89,13 @@ function game(){
         console.log('Wow!It\'s a tie!');
     }
 }
+
+const controls = document.querySelector('.controls');  
+
+
+controls.addEventListener('click',()=>{
+    console.log('yoo')
+})
+
+
+
